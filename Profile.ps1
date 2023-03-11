b0100764b6d4f8789fd942e8643b64ec6064ff07
Connor Peoples <connor@nouseless.tech>
2023-03-11 13:02:32 -0600

    Added git information.

--- Profile.ps1
+++ Profile.ps1
@@ -5,10 +5,30 @@ Function Prompt {
     $CwdLength = $CwdArray.Count
     $Disk = $CwdArray[0]
     $CurrentPath = "$($CwdArray[$CwdLength-2])\$($CwdArray[$CwdLength-1])"
-    $GitBranch = try { Invoke-Command "git branch" -ErrorAction Stop } catch { $Null }
+    $GitBranch = try { 
+        $Branch = Invoke-Expression "git branch" -ErrorAction Stop
+        Write-Host $Branch
+        $Branch.substring(2, $Branch.length-2)
+    } catch { $Null }
+    
+    $GitStatus = try { 
+        $Status = Invoke-Expression "git status -s" -ErrorAction Stop
+        if ($Null -ne $Status) {
+            "Unpushed changes."
+        } else {
+            "Up to date."
+        }
+    } catch { "Error connecting to repo."}
+
     Write-Host -NoNewLine -ForeGroundColor White -BackGroundColor Red " PS: $PSVersion "
     Write-Host -NoNewLine -ForeGroundColor Black -BackGroundColor DarkCyan " $(Get-Date -Format("yyyy-MM-dd hh:mm:ss.fff")) "
-    Write-Host -ForeGroundColor Black -BackGroundColor DarkGreen " $($Disk) $($CurrentPath) "
+    Write-Host -NoNewLine -ForeGroundColor Black -BackGroundColor DarkGreen " $($Disk) $($CurrentPath) "
+    if ($GitBranch) {
+       Write-Host -ForegroundColor Black -BackgroundColor DarkYellow " Branch: $GitBranch Status: $GitStatus"
+    } else {
+        Write-Host ""
+    }
+   
     Write-Host "Don't Panic > " -NoNewLine
     return " "
 }
