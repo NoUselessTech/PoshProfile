b8167237226de020faf105c0c3edb585dff6ac3c
Connor Peoples <connor@peoplesdynamics.com>
2024-01-09 12:33:55 -0600

    Added password creation function.

--- Profile.ps1
+++ Profile.ps1
@@ -1,5 +1,5 @@
 Function Prompt {
-    $PSVersion = $PsVersionTable.PsVersion.ToSTring()
+    $PSVersion = $PsVersionTable.PsVersion.Major.ToSTring()
     $Cwd = (Get-Location).Path
     $CwdArray = $Cwd.split('\')
     $CwdLength = $CwdArray.Count
@@ -20,8 +20,8 @@ Function Prompt {
     } catch { "Error connecting to repo."}
 
     Write-Host -NoNewLine -ForeGroundColor White -BackGroundColor Red " PS: $PSVersion "
-    Write-Host -NoNewLine -ForeGroundColor Black -BackGroundColor DarkCyan " $(Get-Date -Format("yyyy-MM-dd hh:mm:ss.fff")) "
-    Write-Host -NoNewLine -ForeGroundColor Black -BackGroundColor DarkGreen " $($Disk) $($CurrentPath) "
+    Write-Host -NoNewLine -ForeGroundColor Black -BackGroundColor DarkCyan " $(Get-Date -Format("yy-MM-dd hh:mm:ss")) "
+    Write-Host -NoNewLine -ForeGroundColor Black -BackGroundColor DarkGreen " $($Disk):$($CurrentPath) "
     if ($GitBranch) {
        Write-Host -ForegroundColor Black -BackgroundColor DarkYellow " Branch: $GitBranch Status: $GitStatus"
     } else {
@@ -30,4 +30,40 @@ Function Prompt {
    
     Write-Host "Don't Panic > " -NoNewLine
     return " "
+}
+
+Function ConvertFrom-MD {
+    param(
+        $Directory
+    )
+    Write-Host "`r`n Evaluating MD Files."
+    $MDFiles = Get-ChildItem *.md
+
+    If ($Null -ne $Directory) {
+        $MDFiles = Get-ChildItem "$Directory\*.md"
+    }
+
+    ForEach($File in $MDFiles) {
+        Write-Host "   - File: $($File.FullName)"
+        Invoke-Expression "md-to-pdf '$($File.FullName)'"
+    }
+}
+
+Function Create-Password {
+    param (
+        $Length
+    )
+
+    $PwLength = 14
+    if ($Length -gt 0) {
+        $PwLength = $Length
+    }
+
+    $Output = ''
+
+    While($Output.length -lt $PwLength) {
+        $Output += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-='.ToCharArray() | Get-Random
+    }
+
+    return $Output
 }
